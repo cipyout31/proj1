4019fd0598a57504ae3db92812e0d078040b18a6
Harsha Vardhan Devarakonda <chinniharshavardhan@gmail.com>
2024-11-08 07:10:13 +0530

    second

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      harsha vardhan
+    </div>
+  )
+}
+
+export default Navbar
